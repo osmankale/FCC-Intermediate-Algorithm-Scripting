28a6a5a5e1f5a612c97a68b134f696acb62b0efc
Osman Kale <osman.kale@protonmail.com>
2017-02-17 19:04:13 +0000

    new commit

--- Sum All Numbers in a Range.js
+++ Sum All Numbers in a Range.js
@@ -1,12 +1,17 @@
 function sumAll(arr) {
-  var min = Math.min(arr);
-  var max = Math.max(arr);
+  var min = Math.min(arr[0],arr[1]);
+  var max = Math.max(arr[0],arr[1]);
   var newArray = [];
-  for (var i = min; i < max; i++ ) {
+  var total = 0;
+  for (var i = min; i <= max; i++ ) {
       newArray.push(i);
   }
   
-  return newArray;
+  for(var j = 0; j < newArray.length; j++)
+{
+    total = total + newArray[j];
+}
+  return total;
 }
 
 sumAll([1, 4]);
