eda6f73e84e7ee20ad57771c8b6a2f668a8a173b
Osman Kale <osman.kale@protonmail.com>
2017-02-19 11:12:33 +0000

    last commit

--- Diff Two Arrays.js
+++ Diff Two Arrays.js
@@ -1,18 +1,20 @@
 function diffArray(arr1, arr2) {
-  var newArr = [];
+  var newArray = [];
+  var arr3 = arr1.concat(arr2);
+  var sorted = arr3.sort(); 
   
-  var x=0;
-  for (var i = 0; i < arr1.length; i++ ) {
-      for (var j = 0; j < arr2.length; j++) {
-          if (arr1[i] == arr2[j]) {x= 1;break;}
-        }
-        
-        newArr.push(arr1[i]);
+  function compare() {
+    for (var i = 0; i < sorted.length; i++) {
+      for (var j = 0; j < sorted.length; j++) {
+        sorted[i] !== sorted[j];
+        newArray.push(sorted[j]);
       }
+    }
+  }
   
-   
-   
-  return x;
+ 
+   return newArray;
 }
 
-diffArray([1, 2, 3, 4, 5], [1, 2, 3, 5]);
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
